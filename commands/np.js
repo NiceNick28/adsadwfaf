04b4a8190df895ab69e64a4bc0d5a6123847ab04
Nicebot <76829227+NiceNick28@users.noreply.github.com>
2021-07-25 14:17:29 +0700

    Add files via upload

---
+++ commands/np.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.run = async (client, message) => {
+  const channel = message.member.voice.channel;
+  if (!channel)
+    return message.channel.send(
+      "You must Join a voice channel before using this command!"
+    );
+  let queue = message.client.queue.get(message.guild.id);
+  if (!queue)
+    return message.channel.send(
+      new MessageEmbed()
+        .setColor("RED")
+        .setDescription(":x: There are no songs playing in this server")
+    );
+  message.channel.send(
+    new MessageEmbed()
+      .setAuthor(
+        "Now Playing",
+        "https://cdn.discordapp.com/emojis/790122169341837332.gif?v=1"
+      )
+      .setColor("BLUE")
+      .setDescription(
+        queue.queue[0].name +
+          " Requested By: " +
+          "<@" +
+          queue.queue[0].requested +
+          ">"
+      )
+      .setThumbnail(queue.queue[0].thumbnail)
+      .setFooter("There are " + queue.queue.length + " songs in queue")
+  );
+};
---
+++ events/ready.js
@@ -0,0 +1,3 @@
+module.exports = (client) => {
+  console.log("[NICE API]: Logged In As " + client.user.tag);
+};
